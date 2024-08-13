87bc1140bb90d2da322e6628b7a39f9c29f3be48
Artsiom Voitas <artsiom.voitas@gmail.com>
2024-08-13 18:32:43 +0300

    added logic for cross and zero renders onclick

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
 import { useState } from 'react';
+import { BiReset } from 'react-icons/bi';
 import Cell from './components/Cell';
 
+export interface FieldCell {
+    id: number;
+    value: 'cross' | 'zero' | 'empty';
+}
+type Move = 'cross' | 'zero';
+
 function App() {
-    const initialCells: string[] = ['', '', '', '', '', '', '', '', ''];
-    const [cells, setCells] = useState<string[]>(initialCells);
+    const initialCells: FieldCell[] = [
+        { id: 0, value: 'empty' },
+        { id: 1, value: 'empty' },
+        { id: 2, value: 'empty' },
+        { id: 3, value: 'empty' },
+        { id: 4, value: 'empty' },
+        { id: 5, value: 'empty' },
+        { id: 6, value: 'empty' },
+        { id: 7, value: 'empty' },
+        { id: 8, value: 'empty' }
+    ];
+    const [cells, setCells] = useState<FieldCell[]>(initialCells);
+    const [currentMove, setCurrentMove] = useState<Move>('cross');
+
+    function handleClick(fieldCell: FieldCell): void {
+        if (fieldCell.value === 'empty') {
+            setCells(
+                cells.map((cell) =>
+                    fieldCell.id === cell.id ? { ...cell, value: currentMove } : cell
+                )
+            );
+            if (currentMove === 'cross') {
+                setCurrentMove('zero');
+            } else setCurrentMove('cross');
+        }
+        console.log(cells);
+    }
     return (
-        <main className="mx-5 my-96 flex h-full justify-center">
+        <main className="container flex flex-col items-center justify-center">
+            <div className="mb-80 mt-6 flex w-full justify-end">
+                <BiReset
+                    size={36}
+                    color={'blue'}
+                    onClick={() => setCells(initialCells)}
+                    className="mb-9 cursor-pointer"
+                />
+            </div>
             <div className="grid w-fit grid-cols-3 justify-center justify-items-center border border-blue-400">
-                {cells.map((cell, index) => (
+                {cells.map((cell) => (
                     <Cell
-                        key={index}
-                        id={index}
-                        value={cell}
+                        onClick={() => handleClick(cell)}
+                        key={cell.id}
+                        id={cell.id}
+                        value={cell.value}
                     />
                 ))}
             </div>
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,18 +1,29 @@
-interface CellProps {
-    value: string;
-    id: number;
+import { FieldCell } from '../App';
+import { RxCross2 } from 'react-icons/rx';
+import { GiCircle } from 'react-icons/gi';
+
+interface CellsProps extends FieldCell {
+    onClick: () => void;
 }
 
-function Cell({ value, id }: CellProps) {
-    function handleClick(): void {
-        console.log(id);
-    }
+function Cell({ value, id, onClick }: CellsProps) {
     return (
         <button
-            onClick={handleClick}
+            onClick={onClick}
             id={`${id}`}
-            className="flex h-20 w-20 items-center justify-center border border-blue-400">
-            {id}
+            className="flex h-20 w-20 items-center justify-center border border-blue-400 hover:border-blue-600 hover:bg-slate-300">
+            {value === 'cross' && (
+                <RxCross2
+                    size={72}
+                    color="blue"
+                />
+            )}
+            {value === 'zero' && (
+                <GiCircle
+                    size={72}
+                    color="blue"
+                />
+            )}
         </button>
     );
 }
